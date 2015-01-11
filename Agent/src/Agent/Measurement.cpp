40d212aba43af93fb0a36947d17bc8f43c3a9472
root <root@michal-VirtualBox>
2015-01-11 04:17:21 +0100

    new class for measurement in given interval

---
+++ Agent/src/Agent/Measurement.cpp
@@ -0,0 +1,18 @@
+/*
+ * Measurement.cpp
+ *
+ *  Created on: Jan 11, 2015
+ *      Author: root
+ */
+
+#include "Measurement.h"
+
+Measurement::Measurement() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Measurement::~Measurement() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Agent/src/Agent/Measurement.h
@@ -0,0 +1,17 @@
+/*
+ * Measurement.h
+ *
+ *  Created on: Jan 11, 2015
+ *      Author: root
+ */
+
+#ifndef SRC_AGENT_MEASUREMENT_H_
+#define SRC_AGENT_MEASUREMENT_H_
+
+class Measurement {
+public:
+	Measurement();
+	virtual ~Measurement();
+};
+
+#endif /* SRC_AGENT_MEASUREMENT_H_ */
